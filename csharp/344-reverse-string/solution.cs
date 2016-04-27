4ccf307f54b30e87d495edb65660884d3dab36d8
alexinea <forerunner@outlook.com>
2016-04-27 15:31:06 +0800

    leetcode-algorithms-344 by csharp

---
+++ csharp/344-reverse-string/solution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine(ReverseString("Hello World"));
+            Console.ReadLine();
+        }
+
+        static string ReverseString(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+                return string.Empty;
+
+            StringBuilder sb = new StringBuilder();
+
+            for (var i = s.Length - 1; i >= 0; i--)
+            {
+                sb.Append(s[i]);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
