a02c476760bc3c62fb72ca11af4056390d905d1d
alexinea <forerunner@outlook.com>
2016-04-27 15:57:29 +0800

    leetcode-algorithms-339 by csharp

---
+++ csharp/339-nested-list-weight-sum/solution.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object[] nums = { 1, 2, 5, new object[] { 1, 3, 5, new object[] { 1, 5 } }, 4, 1, 5 };
+            Console.WriteLine(GetSum(nums)); //=> 33
+            Console.ReadLine();
+        }
+
+        static int GetSum(object[] nums)
+        {
+            List<int> all = new List<int>();
+            for (var i = 0; i < nums.Length; i++)
+            {
+                object obj = nums[i];
+                var type = obj.GetType();
+                if (type == typeof(int))
+                    all.Add(Int32.Parse(obj.ToString()));
+                else if (type.IsArray)
+                {
+                    object[] objs = obj as object[];
+                    if (objs != null)
+                        all.Add(GetSum(objs));
+                }
+            }
+            return all.Sum();
+        }
+
+
+    }
+}
