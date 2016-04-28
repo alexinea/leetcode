90d3a05e6cdc5ddeb6a30e66351929846e034b6a
alexinea <forerunner@outlook.com>
2016-04-28 15:12:58 +0800

    leetcode-algorithms-226 by csharp

---
+++ csharp/226-invert-binary-tree/solution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+
+        static TreeNode InvertTree(TreeNode root)
+        {
+            if (root == null)
+                return null;
+
+            TreeNode mid = root.left;
+            root.left = root.right;
+            root.right = mid;
+
+            if (root.left != null)
+                root.left = InvertTree(root.left);
+
+            if (root.right != null)
+                root.right = InvertTree(root.right);
+
+            return root;
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+
+}
