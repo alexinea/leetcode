49d10352c820e0625058bbcbc19d415bc3f1a5dc
alexinea <forerunner@outlook.com>
2016-04-29 14:41:11 +0800

    leetcode-algorithms-326 by csharp

---
+++ csharp/326-power-of-three/solution.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+
+        static bool IsPowerOfThree(int n)
+        {
+            var m = Math.Log10(n) / Math.Log10(3);
+            return m == (int)m;
+        }
+    }
+}
