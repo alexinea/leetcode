126fc46d0b1ad0d0b6c313bd8d715615c4229a81
alexinea <forerunner@outlook.com>
2016-05-04 10:42:21 +0800

    leetcode-algorithms-347 by csharp

---
+++ csharp/347-top-k-frequent-elements/solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+
+        static IList<int> TopKFrequent(int[] nums, int k)
+        {
+            Dictionary<int, int> d = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var key = nums[i];
+                if (d.ContainsKey(key))
+                    d[key]++;
+                else
+                    d.Add(key, 0);
+            }
+            return d.OrderByDescending(o => o.Value).Select(s => s.Key).Take(k).ToList();
+        }
+    }
+}
