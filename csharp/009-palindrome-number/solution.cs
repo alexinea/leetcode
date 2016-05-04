80ce48fee9a8810f0685ce4509ebab0b1fc894b8
alexinea <forerunner@outlook.com>
2016-05-04 17:27:13 +0800

    leetcode-algorithms-009 by csharp

---
+++ csharp/009-palindrome-number/solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPalindrome(0));//t
+            Console.WriteLine(IsPalindrome(1));//t
+            Console.WriteLine(IsPalindrome(11));//t
+            Console.WriteLine(IsPalindrome(-1));//f
+            Console.WriteLine(IsPalindrome(12321));//t
+            Console.ReadLine();
+        }
+
+        static bool IsPalindrome(int x)
+        {
+            if (x < 0 || (x > 0 && x % 10 == 0))
+                return false;
+
+            long n = 0;
+            long m = x;
+            while (m >= 1)
+            {
+                n = n * 10 + m % 10;
+                m /= 10;
+            }
+
+            return n == x;
+        }
+    }
+}
