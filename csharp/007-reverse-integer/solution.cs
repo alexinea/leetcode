4e5cf1b4ef1c45af13383896d412da7acee8ac8e
alexinea <forerunner@outlook.com>
2016-05-04 17:15:27 +0800

    leetcode-algorithms-007 by csharp

---
+++ csharp/007-reverse-integer/solution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Reverse(123));
+            Console.WriteLine(Reverse(-123));
+            Console.WriteLine(Reverse(-2147483648));
+            Console.ReadLine();
+        }
+
+        static int Reverse(int x)
+        {
+            if (x > -10 && x < 10)
+                return x;
+
+            bool isNegative = x < 0;
+            long abs = Math.Abs((long)x);
+            long ret = 0;
+
+            while (abs >= 1)
+            {
+                ret = ret * 10 + abs % 10;
+                abs /= 10;
+            }
+
+            if (ret > Int32.MaxValue)
+                return 0;
+
+            return isNegative ? (int)ret * -1 : (int)ret;
+        }
+    }
+}
