c733b7f96f4edef03e613a23a9198b8da8266a4c
alexinea <forerunner@outlook.com>
2016-05-04 16:58:10 +0800

    leetcode-algorithms-006 by csharp

---
+++ csharp/006-zigzag-conversion/solution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Convert("ABC", 2));
+            Console.WriteLine(Convert("ABC", 3));
+            Console.ReadLine();
+        }
+
+        static string Convert(string s, int numRows)
+        {
+            if (numRows == 1)
+                return s;
+
+            string[] scolls = new string[numRows];
+            int idx = 0, cur = 0, mid = numRows - 2;
+
+            while (idx < s.Length)
+            {
+                    for (cur = 0; cur < numRows && idx < s.Length; cur++) scolls[cur] += s[idx++];
+                    for (cur = mid; cur > 0 && idx < s.Length; cur--) scolls[cur] += s[idx++];
+            }
+
+            return scolls.Aggregate(string.Empty, (current, item) => current + item);
+        }
+    }
+}
