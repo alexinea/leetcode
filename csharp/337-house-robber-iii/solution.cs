16050d99c7e145ebe9f822067d91471a941e701f
alexinea <forerunner@outlook.com>
2016-05-04 10:24:08 +0800

    leetcode-algorithms-337 by csharp

---
+++ csharp/337-house-robber-iii/solution.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+
+        static int Rob(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+
+            int left = 0;
+            int right = 0;
+            int cur = root.val;
+
+            if (root.left != null)
+            {
+                left = Rob(root.left);
+                cur += Rob(root.left.left) + Rob(root.left.right);
+            }
+
+            if (root.right != null)
+            {
+                right = Rob(root.right);
+                cur += Rob(root.right.left) + Rob(root.right.right);
+            }
+
+            return cur > (left + right) ? cur : (left + right);
+        }
+    }
+}
