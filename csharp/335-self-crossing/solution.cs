34a74160c19dc3103ffb4e9abd4e52819d4c9d6f
alexinea <forerunner@outlook.com>
2016-05-06 16:47:41 +0800

    leetcode-algorithms-335 by csharp

---
+++ csharp/335-self-crossing/solution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+
+        static bool IsSelfCrossing(int[] x)
+        {
+            if (x.Length < 4)
+                return false;
+
+            for (int i = 0; i < x.Length - 3; i++)
+            {
+                if (x[i + 1] > x[i + 3])
+                    continue;
+
+                if (i < x.Length - 3 && x[i] >= x[i + 2] && x[i + 3] >= x[i + 1])
+                    return true;
+
+                if (i < x.Length - 4 && x[i + 1] == x[i + 3] && x[i] + x[i + 4] >= x[i + 2])
+                    return true;
+
+                if (i < x.Length - 5 && x[i + 1] + x[i + 5] >= x[i + 3] && x[i] + x[i + 4] >= x[2] && x[i + 1] < x[i + 3] && x[i + 4] < x[i + 2])
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ csharp/335-self-crossing/solution2.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+
+        static bool IsSelfCrossing(int[] x)
+        {
+            Hashtable ht = new Hashtable();
+            //North -> xFun is  0 and yFun is  1   cur_ctrl % 4 == 0
+            //West --> xFun is -1 and yFun is  0   cur_ctrl % 4 == 1
+            //South -> xFun is  0 and yFun is -1   cur_ctrl % 4 == 2
+            //East --> xFun is  1 and yFun is  0   cur_ctrl % 4 == 3
+            int xFun = 0;
+            int yFun = 1;
+
+            //准备
+            int cur_x = 0, cur_y = 0;
+            ht.Add(string.Format("x{0}y{1}", cur_x, cur_y), 0);
+
+            //开始
+            for (int i = 0; i < x.Length; i++)
+            {
+                // i 就是 cur_ctrl
+                switch (i % 4)
+                {
+                    case 0: xFun = 0; yFun = 1; break;
+                    case 1: xFun = -1; yFun = 0; break;
+                    case 2: xFun = 0; yFun = -1; break;
+                    case 3: xFun = 1; yFun = 0; break;
+                }
+
+                var step = x[i];
+                for (var j = step; j > 0; j--)
+                {
+                    cur_x += xFun;
+                    cur_y += yFun;
+                    var point = string.Format("x{0}y{1}", cur_x, cur_y);
+                    if (ht.ContainsKey(point))
+                        return true;
+
+                    ht.Add(point, 0);
+                }
+            }
+
+            return false;
+        }
+    }
+}
